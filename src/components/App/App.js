f2deb5467a81cd2787f71408509fb6f1e12b4519
Dana Truong <tramtramphotos@gmail.com>
2023-01-02 14:40:43 -0600

    Initial commit

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,19 @@ import { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      tricks: []
+    }
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:3001/api/v1/tricks')
+      .then(data => data.json())
+      .then(data => this.setState({ tricks: data }))
+  }
+
   render() {
     return (
       <div className="App">
